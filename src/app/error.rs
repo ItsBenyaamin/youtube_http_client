5ba12a20e4a06fb4f7f9eeb42901cdf3a525b0f4
Benyamin Eskandari <me@benyaamin.com>
2022-11-02 00:06:42 +0330

    ep 02

--- src/app/error.rs
+++ src/app/error.rs
@@ -4,12 +4,20 @@ use std::fmt::Display;
 #[derive(Debug)]
 pub enum Error {
     UrlParsingError,
+    IoError
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::UrlParsingError => write!(f, "Error occure on parsing URL!"),
+            Error::UrlParsingError => write!(f, "An error occurred on parsing the URL!"),
+            Error::IoError => write!(f, "An error occurred on IO operation!"),
         }
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(_: std::io::Error) -> Self {
+        Error::IoError
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use app::error::Error;
+use client::connection::Connection;
 
 mod app;
 mod client;
 
 #[tokio::main]
 async fn main() -> Result<(), Error>{
-    
-
+    let mut connection = Connection::new("http://168.119.172.64").await?;
+    connection.head_request().await?;
     Ok(())
 }
