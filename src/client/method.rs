31db61f1243b9b7df26c6e95a3be077ffa1b7a4c
Benyamin Eskandari <me@benyaamin.com>
2022-11-06 11:12:59 +0330

    ep4

---
+++ src/client/method.rs
@@ -0,0 +1,26 @@
+use std::fmt::Display;
+
+
+pub enum Method {
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    HEAD,
+    OPTIONS,
+    DELETE
+}
+
+impl Display for Method {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Method::GET => write!(f, "GET"),
+            Method::POST => write!(f, "POST"),
+            Method::PUT => write!(f, "PUT"),
+            Method::PATCH => write!(f, "PATCH"),
+            Method::HEAD => write!(f, "HEAD"),
+            Method::OPTIONS => write!(f, "OPTIONS"),
+            Method::DELETE => write!(f, "DELETE"),
+        }
+    }
+}
